a2c71eca4c401f93c78554f80632f6b584a751d8
Sushicat <86925500+Nathans-SushiCat@users.noreply.github.com>
2024-05-12 18:08:05 +0200

    fix: highlight personal score

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -10,7 +10,6 @@ public class Leaderboard : MonoBehaviour
     [SerializeField] private List<TMP_Text> names;
     [SerializeField] private List<TMP_Text> scores;
     [SerializeField] Color UserColor;
-    public static string[] badwords = {"RAWR", "Sushicat"};
 
     public static string publicLeaderboardKey = "65808c598689e94f03eb4d0935e398184f16e720abc618f463a59635b93e2090";
 
@@ -25,7 +24,8 @@ public class Leaderboard : MonoBehaviour
             {
                 names[i].text = msg[i].Username;
                 scores[i].text = msg[i].Score.ToString();
-                if (msg[i].Username.Equals(PlayerPrefs.GetString("UserName")))
+
+                if (msg[i].IsMine())
                 {
                     names[i].color = UserColor;
                     scores[i].color = UserColor;
